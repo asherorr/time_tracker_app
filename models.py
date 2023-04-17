22db446224b32a436d3ec00f896383ec9095c83d
asherorr <asher@mailfence.com>
2023-04-17 19:22:36 -0400

    This commit will import sqlalchemy, create the engine, the session, and the Database model.

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import (create_engine, Column, Integer, String, Date)
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///time_tracker.db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Tracker(Base):
+    __tablename__ = "Time_Spent_And_Stats"
+    id= Column(Integer, primary_key=True)
