d05e19d5efeb1d4b914f883cbe96da5a5f5cf732
asherorr <asher@mailfence.com>
2023-04-27 17:32:37 -0400

    This commit will create a menu function in app.py and handle any user errors.

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from models import (Base, session, Tracker, engine)
 
 
+def menu():
+    while True:
+        print('''
+        \rOPTIONS
+        \r----
+        \r1) Add Event
+        \r2) View Times
+        \r3) See Statistics
+        ''')
+        choice = input("What would you like to do? ")
+        if choice in range(1-4):
+            return choice
+        else:
+            input('''
+                  \rPlease choose only one of the options above.
+                  \rA number from 1-3.
+                  \rPress enter to try again. ''')
+        
+
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
+    menu()
