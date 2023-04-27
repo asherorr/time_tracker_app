1f12a5a8c3aeed8def99a9fa14f50524de9b89cc
asherorr <asher@mailfence.com>
2023-04-27 17:38:08 -0400

    This commit will create an app function in app.py

--- app.py
+++ app.py
@@ -18,6 +18,23 @@ def menu():
                   \rPlease choose only one of the options above.
                   \rA number from 1-3.
                   \rPress enter to try again. ''')
+            
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice == "1":
+            # add event
+            pass
+        elif choice == "2":
+            #view times
+            pass
+        elif choice == "3":
+            #search statistics (num attempted and average)
+            pass
+        else:
+            print("Goodbye!")
+            app_running = False
         
 
 if __name__ == "__main__":
